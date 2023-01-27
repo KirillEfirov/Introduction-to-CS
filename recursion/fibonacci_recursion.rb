533d02714d5008c9a25bd96e7478dedbfb8d9134
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-01-28 01:30:34 +0300

    return array of n+1 Fibonacci numbers

--- recursion/fibonacci_recursion.rb
+++ recursion/fibonacci_recursion.rb
@@ -1,7 +1,10 @@
 def fibs(number)
-    return number if number == 1 || number == 0
+    return [0]   if number == 0
+    return [0,1] if number == 1
 
-    return fibs(number - 1) + fibs(number - 2)
+    array = fibs(number - 1)
+
+    array << (array[-2] + array[-1])
 end
 
 p fibs(0)
