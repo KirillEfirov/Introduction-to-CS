102017a08754c4eb95e8924fcdd1fd302e46600c
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-01-31 17:10:15 +0300

    test size method

--- data_structures/main.rb
+++ data_structures/main.rb
@@ -7,14 +7,17 @@ list.append 49
 list.append 84
 list.append 4
 
+puts list.size
 puts list
 
 list.prepend 49
 list.prepend 84
 list.prepend 4
 
+puts list.size
 puts list
 
 list2 = LinkedList.new
 
+puts list2.size
 puts list2
