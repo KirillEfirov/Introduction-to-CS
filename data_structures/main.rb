7ffbe2b72c04cebe5d82b3275e94bc748afc56ce
Kirill Shishko <shishkokirill.fudosin@gmail.com>
2023-01-31 17:27:07 +0300

    test head and tail methods

--- data_structures/main.rb
+++ data_structures/main.rb
@@ -6,18 +6,25 @@ list = LinkedList.new
 list.append 49
 list.append 84
 list.append 4
+puts "LIST: #{list}
+      LIST SIZE: #{list.size}
+      LIST HEAD: #{list.head}
+      LIST TAIL: #{list.tail}"
 
-puts list.size
-puts list
+puts
 
 list.prepend 49
 list.prepend 84
 list.prepend 4
+puts "LIST: #{list}
+      LIST SIZE: #{list.size}
+      LIST HEAD: #{list.head}
+      LIST TAIL: #{list.tail}"
 
-puts list.size
-puts list
+puts
 
 list2 = LinkedList.new
-
-puts list2.size
-puts list2
+puts "LIST: #{list2}
+      LIST SIZE: #{list2.size}
+      LIST HEAD: #{list2.head}
+      LIST TAIL: #{list2.tail}"
